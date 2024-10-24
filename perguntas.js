371bd487d7ff78997c1295768e8318a5e7eabc34
caduedu17 <0000109375249xsp@al.educacao.sp.gov.br>
2024-10-24 18:13:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual é o resultado de 9 x 6?',
+    'O resultado é 54.'
+)
+
+criaCartao(
+    'Português',
+    'O que é um substantivo?',
+    'Um substantivo é a palavra que nomeia pessoas, lugares, coisas ou ideias.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é a função do sistema circulatório?',
+    'A função do sistema circulatório é transportar sangue, oxigênio e nutrientes para as células do corpo.'
+)
+
+criaCartao(
+    'História',
+    'O que foi a Revolução Industrial?',
+    'A Revolução Industrial foi um período de grandes mudanças econômicas e sociais que começou no final do século XVIII, caracterizado pela transição de uma economia agrária para uma economia industrial.'
+)
